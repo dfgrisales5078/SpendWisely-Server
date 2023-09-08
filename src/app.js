a851924d82376bfb931a8c54b93e199513328424
Diego Grisales <54412831+dfgrisales5078@users.noreply.github.com>
2023-09-07 23:32:15 -0400

    Temp changes to deploy v1 to ec2

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 require("dotenv").config({ path: "../.env" });
 const express = require("express");
+const morgan = require("morgan");
 const applyMiddlewares = require("./middlewares");
 const transactionRoutes = require("./routes/transactionsRoutes");
 const authenticationRoutes = require("./routes/loginRoutes");
 const registerRoutes = require("./routes/registerRoutes");
 
 const app = express();
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms")
+);
 const PORT = 4000;
 
 applyMiddlewares(app);
@@ -14,6 +18,6 @@ app.use(transactionRoutes);
 app.use(authenticationRoutes);
 app.use(registerRoutes);
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
